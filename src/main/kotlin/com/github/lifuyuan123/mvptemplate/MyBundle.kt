7a731e7e41cbc3c8c76d33d3211aa9f217b6b90d
GitHub Action <action@github.com>
2022-03-31 05:55:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/lifuyuan123/mvptemplate/MyBundle.kt
+++ src/main/kotlin/com/github/lifuyuan123/mvptemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lifuyuan123.mvptemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lifuyuan123/mvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lifuyuan123/mvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lifuyuan123.mvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lifuyuan123.mvptemplate.MyBundle
 
 class MyApplicationService {
 
